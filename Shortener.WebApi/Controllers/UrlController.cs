27e546c50d357475bc1d24ed05a55b3255b69d2a
oleh-dumankyi <dasgo121@gmail.com>
2023-02-08 14:17:58 +0200

    Added basic controller logic

---
+++ Shortener.WebApi/Controllers/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shortener.Application.Urls.Queries.GetUrlsList;
+
+namespace Shortener.WebApi.Controllers
+{
+    public class UrlController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<UrlsListVm>> GetAll()
+        {
+            var query = new GetUrlListQuery()
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
